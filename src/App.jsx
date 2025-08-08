d5b5e84b51ad0cec197266dc8c30c3d2109e338b
Dima Tsoy <dimktsoy@icloud.com>
2025-08-08 15:54:40 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const handleClick = () => {
+  window.gtag('event', 'thank_you', {
+    event_category: 'cta',
+    event_label: 'click gtag'
+  });
+};
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +36,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <button type='button' onClick={handleClick}>click gtag</button>
+      </div>
     </>
   )
 }
